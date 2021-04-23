a63e9bb469d875e87b4b2a95950364a58f3b7553
Manuel Fabri <manuelnfabri@gmail.com>
2021-04-23 19:20:16 -0300

    Updates

---
+++ precache-manifest.3d8cd77b458f8ed3a998466a15aefe05.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2189858288835b328d9ca4973b5d3517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37a584af56ce601f9c86",
+    "url": "/static/css/2.adc6609b.chunk.css"
+  },
+  {
+    "revision": "d0261bab8ed4ebcd06f4",
+    "url": "/static/css/main.693fd8a5.chunk.css"
+  },
+  {
+    "revision": "37a584af56ce601f9c86",
+    "url": "/static/js/2.20cc6f7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.20cc6f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0261bab8ed4ebcd06f4",
+    "url": "/static/js/main.9db736dd.chunk.js"
+  },
+  {
+    "revision": "6b416210ee914163559d",
+    "url": "/static/js/runtime-main.5e66ba5e.js"
+  },
+  {
+    "revision": "4a97e67b8ec44fc03ef3e39be8f8bc6c",
+    "url": "/static/media/avatar.4a97e67b.png"
+  },
+  {
+    "revision": "492b7728939940590e479345e3a47071",
+    "url": "/static/media/hero-image.492b7728.svg"
+  },
+  {
+    "revision": "ba3a87c195d5cfd2603a5c721db9c4a6",
+    "url": "/static/media/logo.ba3a87c1.png"
+  }
+]);
